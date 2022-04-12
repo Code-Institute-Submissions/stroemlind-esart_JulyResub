8b3824fea9748571e51d16c0e0f60c0e13afd789
stroemlind <elin.stromlind@gmail.com>
2022-04-12 10:20:22 +0000

    Add Poster and Motive models and set up admin.py for posters app

--- posters/admin.py
+++ posters/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Motive, Poster
 
-# Register your models here.
+
+class PosterAdmin(admin.ModelAdmin):
+    """ An admin class for Poster products """
+    list_display = (
+        'name',
+        'motive',
+        'description',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+class MotiveAdmin(admin.ModelAdmin):
+    """ An admin class for Motive/Categories """
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Poster, PosterAdmin)
+admin.site.register(Motive, MotiveAdmin)
--- posters/models.py
+++ posters/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Motive(models.Model):
+    """ The model for motives / Categories """
+
+    name = models.CharField(max_length=200)
+    friendly_name = models.CharField(max_length=200, null=True, blank=True)
+
+    class Meta:
+        """ Defines the model name in plural """
+        verbose_name_plural = 'Motives'
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        """ Returns the friendly name of a motive """
+        return self.friendly_name
+
+
+class Poster(models.Model):
+    """ The model for the Poster product """
+    name = models.CharField(max_length=200)
+    motive = models.ForeignKey(
+        'Motive', null=True, blank=True, on_delete=models.SET_NULL)
+    description = models.TextField()
+    size = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    image = models.ImageField(null=True, blank=True)
+    like = models.ManyToManyField(
+        User, related_name='poster_like', blank=True)
+
+    def __str__(self):
+        return self.name
