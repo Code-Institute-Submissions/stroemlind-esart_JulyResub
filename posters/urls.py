4c3149c50ffb117759c0df1ec33c2e9bd189d071
stroemlind <elin.stromlind@gmail.com>
2022-04-13 12:49:30 +0000

    Add view, url and template for poster detail page

--- posters/urls.py
+++ posters/urls.py
@@ -4,5 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
     path('about-us', views.about_us, name='about-us'),
-    path('posters/', views.posters, name='posters'),
+    path('posters/', views.posters_all_view, name='posters'),
+    path(
+        'posters/<int:poster_id>/',
+        views.poster_detail,
+        name='poster-detail'
+    ),
 ]
--- posters/views.py
+++ posters/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, get_object_or_404
 
 from .models import Poster, Motive
 
-# Create your views here.
+
 def index(request):
     """ A view to render the index page """
     return render(request, 'index.html')
@@ -13,7 +13,7 @@ def about_us(request):
     return render(request, 'about-us.html')
 
 
-def posters(request):
+def posters_all_view(request):
     """ A view to render the Poster product page """
 
     posters = Poster.objects.all()
@@ -24,3 +24,32 @@ def posters(request):
     }
 
     return render(request, template, context)
+
+
+def poster_detail(request, poster_id):
+    """ A view to show individual poster details """
+
+    poster = get_object_or_404(Poster, pk=poster_id)
+    template = 'posters/poster-detail.html'
+
+    context = {
+        'poster': poster,
+    }
+
+    return render(request, template, context)
+
+# class LikeView(View):
+#     """
+#     The view for likes
+#     """
+#     def post(self, request, pk, *args, **kwargs):
+#         """
+#         The function to determine the view if a user has liked a post or not
+#         """
+#         post = get_object_or_404(Post, pk=pk)
+#         if post.likes.filter(id=request.user.id).exists():
+#             post.likes.remove(request.user)
+#         else:
+#             post.likes.add(request.user)
+
+#         return HttpResponseRedirect(reverse('post-detail', args=[pk]))
