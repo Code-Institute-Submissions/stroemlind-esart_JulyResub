7863eeb66f170161d66d789bed7650bd660211f4
stroemlind <elin.stromlind@gmail.com>
2022-04-20 10:30:17 +0000

    Set up Order and OrderLineItem models, signals and admin.py for checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Order, OrderLineItem
 
-# Register your models here.
+
+class OrderLineAdminInline(admin.TabularInline):
+    """ The Adminclass for the OrderLineItem Model"""
+    model = OrderLineItem
+    readonly_fields = ('lineitem_total',)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    """ The Adminclass for the Order Model"""
+    inlines = (OrderLineAdminInline,)
+
+    readonly_fields = ('order_number', 'date',
+                       'delivery_cost', 'order_total',
+                       'total_cost',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'street_address1',
+              'street_address2', 'postcode', 'county',
+              'town_or_city', 'country', 'delivery_cost',
+              'order_total', 'total_cost',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'order_total', 'delivery_cost',
+                    'total_cost',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
--- esart/settings.py
+++ esart/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'home',
     'posters',
     'cart',
+    'checkout',
 ]
 
 MIDDLEWARE = [
