ef650aa1bbb4015a9738bada0b76096f0ea195d1
stroemlind <elin.stromlind@gmail.com>
2022-04-22 08:53:37 +0000

    Add stripe payment functionallity

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 import stripe
 
 from posters.models import Poster
+from cart.context import cart_contents
 from .models import OrderLineItem
 from .forms import PosterOrderForm
 
@@ -17,52 +18,23 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     client_secret = settings.CLIENT_SECRET
 
-    if request.method == 'POST':
-        poster_order_form = PosterOrderForm(request.POST)
+    cart = request.session.get('cart', {})
 
-        if poster_order_form.is_valid():
-            order = poster_order_form.save(commit=False)
-            order.date = timezone.now()
-            order.save()
+    if not cart:
+        messages.error(request, "There is nothing in your shopping cart at this moment")
+        return redirect(reverse('posters'))
 
-            cart = request.session.get('cart', {})
-            total = 0
-            for id, quantity in cart.items():
-                poster = get_object_or_404(Poster, pk=id)
-                total += quantity * poster.price
-                order_line_item = OrderLineItem(
-                    order=order,
-                    poster=poster,
-                    quantity=quantity
-                )
-                order_line_item.save()
+    current_cart = cart_contents(request)
+    total = current_cart['total_cost']
+    amount_total = round(total * 100)
+    stripe_currency = 'EUR'
+    stripe.api_key = client_secret
+    intent = stripe.PaymentIntent.create(
+        amount=amount_total,
+        currency=stripe_currency,
+    )
 
-            try:
-                customer = stripe.Charge.create(
-                    amount=int(total * 100),
-                    currency='EUR',
-                    description=request.user.email,
-                    card=payment_form.cleaned_data['stripe_id'],
-                )
-            except stripe.error.CardError:
-                messages.error(request, 'Your Card was Declined')
-
-            if customer.paid:
-                messages.error(request, 'You have successfully paid')
-                request.session['cart'] = {}
-                return redirect(reverse('home'))
-            else:
-                messages.error(request, 'Unable to take payment')
-        else:
-            print(payment_form.errors)
-            messages.error(request, 'We were unable to take a payment with that card')
-    else:
-        # poster_order_form = PosterOrderForm()
-        cart = request.session.get('cart', {})
-
-        if not cart:
-            messages.error(request, "There is nothing in your shopping cart at this moment")
-            return redirect(reverse('posters'))
+    print(intent)
 
     poster_order_form = PosterOrderForm()
     template = 'checkout/checkout.html'
@@ -70,7 +42,7 @@ def checkout(request):
     context = {
         'poster_order_form': poster_order_form,
         'stripe_public_key': stripe_public_key,
-        'client_secret': client_secret,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
