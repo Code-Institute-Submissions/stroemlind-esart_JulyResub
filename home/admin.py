4938358e79fb1782b8201436b301a678907475d2
stroemlind <elin.stromlind@gmail.com>
2022-04-27 20:53:45 +0000

    Add NewsletterSubscriber model and setup admin in home app

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import NewsletterSubscriber
 
-# Register your models here.
+
+class NewsletterAdmin(admin.ModelAdmin):
+    """
+    Admin model to handle newsletter subscribers
+    from the admin panel
+    """
+    list_display = ('email', 'subscribe_date')
+
+    ordering = ('subscribe_date',)
+
+
+admin.site.register(NewsletterSubscriber, NewsletterAdmin)
--- home/models.py
+++ home/models.py
@@ -1,4 +1,13 @@
 from django.db import models
 
 
+class NewsletterSubscriber(models.Model):
+    """
+    A model to collect email for Newsletter 
+    subcriptions
+    """
+    email = models.EmailField()
+    subscribe_date = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.email
