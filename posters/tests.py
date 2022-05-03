3a076949e88681d1fcd6ef3c065c2b83a3f113fe
stroemlind <elin.stromlind@gmail.com>
2022-05-03 09:07:36 +0000

    Add TestViews to posters app

--- posters/tests.py
+++ posters/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from .models import Poster
 
-# Create your tests here.
+
+class TestViews(TestCase):
+    """ Test the code for the views """
+    @classmethod
+    def setUpTestData(cls):
+
+        Poster.objects.create(
+            name='Test Name',
+            description='Test and test',
+            size=True,
+            quantity=1,
+            price=50.00,
+            image='placeholder'
+        )
+
+    def test_get_posters_page(self):
+        """ Test to get the right template for the view """
+        response = self.client.get('/posters/posters/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(
+            response,
+            'posters/posters-page.html',
+            'base.html'
+        )
