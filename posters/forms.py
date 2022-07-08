b6390b8b28e24405c3f3cc1ef2c81412f92be09e
stroemlind <elin.stromlind@gmail.com>
2022-07-08 10:48:46 +0000

    Set custom style for PosterForm

--- posters/forms.py
+++ posters/forms.py
@@ -13,3 +13,39 @@ class PosterForm(forms.ModelForm):
         """
         model = Poster
         exclude = ('like', )
+
+        widgets = {
+                'name': forms.TextInput(attrs={
+                    'class': 'form-control request-form mb-2',
+                    'placeholder': 'Enter Poster Name Here',
+                    'aria-label': 'Enter Poster Name',
+                }),
+                'motive': forms.Select(attrs={
+                    'class': 'form-control request-form mb-2',
+                }),
+                'description': forms.Textarea(attrs={
+                    'class': 'form-control request-form mb-2',
+                    'placeholder': 'Enter Poster Description Here',
+                    'aria-label': 'Enter Poster Description',
+                }),
+                'size': forms.NullBooleanSelect(attrs={
+                    'class': 'form-control request-form mb-2',
+                    'placeholder': 'Select Size',
+                    'aria-label': 'Select Size',
+                }),
+                'quantity': forms.NumberInput(attrs={
+                    'class': 'form-control request-form mb-2',
+                    'placeholder': 'Enter Quantity',
+                    'aria-label': 'Enter Quantity',
+                }),
+                'price': forms.NumberInput(attrs={
+                    'class': 'form-control request-form mb-2',
+                    'placeholder': 'Enter Price Here',
+                    'aria-label': 'Enter Price',
+                }),
+                'image': forms.TextInput(attrs={
+                    'class': 'form-control rounded-0 border-dark mb-2',
+                    'placeholder': 'Enter Image Here e.g image.jpeg',
+                    'aria-label': 'Enter Image',
+                }),
+        }
