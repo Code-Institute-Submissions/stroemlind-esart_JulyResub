f68d377ace3996be9a5567eff90ec65367264621
stroemlind <elin.stromlind@gmail.com>
2022-07-08 07:15:39 +0000

    Style add-poster template and PosterForm

--- posters/forms.py
+++ posters/forms.py
@@ -3,7 +3,13 @@ from .models import Poster
 
 
 class PosterForm(forms.ModelForm):
-    """"""
+    """
+    The PosterForm model to add
+    new posters to the shop
+    """
     class Meta:
+        """
+        Tells django which lines to use from the model
+        """
         model = Poster
         exclude = ('like', )
