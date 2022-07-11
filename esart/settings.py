d7ec7c4851fc27893fbe776da5f848d1164b9093
stroemlind <elin.stromlind@gmail.com>
2022-07-11 21:06:29 +0000

    Remove comment out code from poster-page and poster-detail templates

--- esart/settings.py
+++ esart/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
-    # 'cloudinary_storage',
     'cloudinary',
     'crispy_forms',
     'home',
